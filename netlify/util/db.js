ddda219fe339280d62c20252c6d2436d90d15d36
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-02 11:32:30 -0800

    update message

--- netlify/util/db.js
+++ netlify/util/db.js
@@ -3,6 +3,7 @@ const { Client } = require("pg");
 async function query(text, params) {
   const client = new Client({
     connectionString: process.env.NETLIFY_DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   });
 
   await client.connect();
