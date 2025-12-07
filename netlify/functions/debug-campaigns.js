edf7f5d971ff1ddcc63c8fc3b8807a5edf3dc2a0
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-07 12:43:25 -0800

    update message

--- netlify/functions/debug-campaigns.js
+++ netlify/functions/debug-campaigns.js
@@ -1,27 +1,13 @@
-// netlify/functions/debug-campaigns.js
-
-import { NetlifyRequest, NetlifyResponse } from "@netlify/functions";
-
-export default async function handler(request /*: NetlifyRequest */) {
-  try {
-    // Sanity: prove that this file is running + import works
-    const info = {
+export const handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
       ok: true,
-      message: "debug-campaigns is alive",
-      method: request.method,
-      // request.query is a URLSearchParams in the new API
-      query: Array.from(request.query.keys()),
-      hasBody: !!request.body,
-    };
-
-    return NetlifyResponse.json(info);
-  } catch (err) {
-    return NetlifyResponse.json(
-      {
-        ok: false,
-        error: err.message || "Unknown error",
-      },
-      { status: 500 }
-    );
-  }
-}
+      message: "Classic handler works",
+      method: event.httpMethod,
+      query: event.queryStringParameters,
+      raw: event
+    })
+  };
+};
