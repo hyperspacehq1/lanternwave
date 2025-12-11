d177fc402ff7e97d4b7f5886057dee0868e2e576
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-11 07:36:37 -0800

    update message

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,              // Enables App Router
-    serverActions: true,       // Required for Next 14 server actions
-  },
-};
-
-export default nextConfig;
