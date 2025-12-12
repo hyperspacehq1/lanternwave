8de86b8038e691bc6f1914897584aefbc6cf24bd
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-11 18:47:31 -0800

    update message

--- components/forms/EventForm.jsx
+++ components/forms/EventForm.jsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { cmApi } from "@/lib/cm/client";
+// Import { cmApi } from "@/lib/cm/client";
 
 export default function EventForm({ record, onChange }) {
   const [campaigns, setCampaigns] = useState([]);
