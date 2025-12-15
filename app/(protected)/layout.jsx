b610902523d3597c3f5493fe7703d11eeab4cc1b
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-15 14:42:43 -0800

    update message

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -1,13 +1,15 @@
 import "../globals.css";
-import { requireAuth } from "@/lib/auth";
-import Header from "@/components/Header";
+// import { requireAuth } from "@/lib/auth";
+// import Header from "@/components/Header";
 
 export default async function ProtectedLayout({ children }) {
-// await requireAuth();
+  // await requireAuth();
 
   return (
     <div className="lw-app-root">
+      {/*
       <Header />
+      */}
       <main className="lw-main">
         {children}
       </main>
