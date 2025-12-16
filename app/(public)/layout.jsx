37240c441673efc20a2dcc964f33621a385970f3
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-15 21:36:29 -0800

    update message

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,29 +1,21 @@
 import Header from "@/components/Header";
 import Link from "next/link";
-import "./globals.css";
 
 export const dynamic = "force-dynamic";
 
-export default function RootLayout({ children }) {
+export default function PublicLayout({ children }) {
   return (
-    <html lang="en" className="lw-root">
-      <body>
-        {/* APP HEADER */}
-        <Header />
+    <>
+      <Header variant="public" />
 
-        {/* MAIN APP CONTENT */}
-        <main className="lw-main">
-          {children}
-        </main>
+      {children}
 
-        {/* APP FOOTER */}
-        <footer className="lw-footer">
-          <Link href="/privacy-policy">Privacy Policy</Link>
-          <Link href="/terms">Terms</Link>
-          <Link href="/contact">Contact Us</Link>
-          <Link href="/about">About</Link>
-        </footer>
-      </body>
-    </html>
+      <footer className="lw-footer">
+        <Link href="/privacy-policy">Privacy Policy</Link>
+        <Link href="/terms">Terms</Link>
+        <Link href="/contact">Contact Us</Link>
+        <Link href="/about">About</Link>
+      </footer>
+    </>
   );
 }
