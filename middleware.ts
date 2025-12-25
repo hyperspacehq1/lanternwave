702df0e74e008b1a3c5197664d7d6807c9f59dad
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-25 15:42:49 -0800

    update message

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const session = request.cookies.get("lw_session");
-
-  // Not logged in → redirect
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
+export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
