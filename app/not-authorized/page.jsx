c3ed1325a8d757db4dc514cf98134ec88f64d5da
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-30 10:45:18 -0800

    update message

---
+++ app/not-authorized/page.jsx
@@ -0,0 +1,12 @@
+export default function NotAuthorizedPage() {
+  return (
+    <div style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>🚫 Access Denied</h1>
+      <p>You do not have permission to view this page.</p>
+
+      <p style={{ marginTop: "1rem" }}>
+        If you believe this is an error, please contact an administrator.
+      </p>
+    </div>
+  );
+}
