addee31dd98470f3e6f27b7e9d187d7d6bfecbde
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2025-12-29 23:39:01 -0800

    update message

---
+++ page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>LanternWave is Live</h1>
+      <p>If you see this, your deployment works.</p>
+    </main>
+  );
+}
