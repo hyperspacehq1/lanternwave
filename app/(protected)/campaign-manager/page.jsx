96cfc0448e60833d2da330fbe2044fecfa17bd8f
Brett Gordon <142930808+hyperspacehq1@users.noreply.github.com>
2026-01-14 18:36:33 -0800

    update message

--- app/(protected)/campaign-manager/page.jsx
+++ app/(protected)/campaign-manager/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useMemo, useState } from "react";
+// import React, { useEffect, useMemo, useState } from "react";
 // import { v4 as uuidv4 } from "uuid";
 
 // ❌ TEMP DISABLED FOR DEBUGGING
